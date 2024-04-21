5de057e0a459747c0af30103b40bb7705a7ad62e
Paulo J. <113233649+Pauu1o@users.noreply.github.com>
2024-04-21 14:37:40 +0800

    u

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "./authoption";
+import { authOptions } from "./authOptions";
 import NextAuth from "next-auth";
 export { handler as GET, handler as POST };
 const handler = NextAuth(authOptions);
