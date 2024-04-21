a409687acbfe6afce365013d6f7cd736cfd84ebb
Paulo J. <113233649+Pauu1o@users.noreply.github.com>
2024-04-21 14:35:33 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
-import authOptions from "./authoption";
-
-export default NextAuth(authOptions);
+import { authOptions } from "./authoption";
+import NextAuth from "next-auth";
+export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
